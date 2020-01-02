b0bcbaa85a9754df456fc5df078e04b2c60444bb
khmilevoi <khmilevoi@gmail.com>
2020-01-02 14:40:38 +0200

    feat(TodoManager.js) add todo manager

---
+++ js/TodoManager.js
@@ -0,0 +1,38 @@
+const idGenerator = (() => {
+  let currentId = 0;
+
+  return () => currentId++;
+})();
+
+class TodoManager {
+  constructor() {
+    this.todos = [];
+  }
+
+  add(inner) {
+    const id = idGenerator();
+
+    const todo = {
+      inner,
+      id,
+      completed: false
+    };
+
+    this.todos.push(todo);
+
+    return todo;
+  }
+
+  delete(id) {
+    const index = this.todos.findIndex(todo => todo.id === id);
+
+    return this.todos.splice(index, 1);
+  }
+
+  toggle(id) {
+    const index = this.todos.findIndex(todo => todo.id === id);
+    const currentState = this.todos[index].completed;
+
+    return (this.todos[index].completed = !currentState);
+  }
+}
