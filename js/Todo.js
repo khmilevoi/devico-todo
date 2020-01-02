30474495c7bf77c1684ea6317fb024700f6a3b49
khmilevoi <khmilevoi@gmail.com>
2020-01-02 15:53:59 +0200

    feat(Todo.js) add todo conroller

---
+++ js/Todo.js
@@ -0,0 +1,78 @@
+const actions = {
+  ADD: "todo/add",
+  DELETE: "todo/delete",
+  TOGGLE: "todo/toggle"
+};
+
+class Todo {
+  constructor(todoManager, todosContainer) {
+    this.todoManager = todoManager;
+    this.todosContainer = todosContainer;
+  }
+
+  draw() {
+    this.todosContainer.innerHTML = "";
+
+    this.todoManager.todos.forEach(todo => {
+      const todoContainer = document.createElement("div");
+      todoContainer.classList.add("todo");
+
+      if (todo.completed) {
+        todoContainer.classList.add("completed");
+      }
+
+      const toggleButton = document.createElement("input");
+      toggleButton.classList.add("todo-checkbox");
+      toggleButton.type = "checkbox";
+      toggleButton.checked = todo.completed;
+
+      toggleButton.addEventListener("click", event => {
+        event.preventDefault();
+
+        this.dispatch(actions.TOGGLE, todo.id);
+      });
+
+      const deleteButton = document.createElement("button");
+      deleteButton.classList.add("todo-delete");
+      deleteButton.innerHTML = "delete";
+
+      deleteButton.addEventListener("click", event => {
+        event.preventDefault();
+
+        this.dispatch(actions.DELETE, todo.id);
+      });
+
+      const inner = document.createElement("div");
+      inner.innerHTML = todo.inner;
+
+      todoContainer.append(toggleButton, inner, deleteButton);
+
+      this.todosContainer.append(todoContainer);
+    });
+  }
+
+  dispatch(action, payload) {
+    switch (action) {
+      case actions.ADD: {
+        this.todoManager.add(payload);
+        break;
+      }
+
+      case actions.DELETE: {
+        this.todoManager.delete(payload);
+        break;
+      }
+
+      case actions.TOGGLE: {
+        this.todoManager.toggle(payload);
+        break;
+      }
+
+      default: {
+        break;
+      }
+    }
+
+    this.draw();
+  }
+}
