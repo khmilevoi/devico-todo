dea2b2385849f3027bc0ff17f6649d7ea455fdd8
khmilevoi <khmilevoi@gmail.com>
2020-01-10 14:13:13 +0200

    style() todo

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -8,7 +8,57 @@ export class Todo extends Component {
   }
 
   render() {
-    const todo = createElement('div', { class: 'todo' });
+    const label = createElement(
+      'label',
+      {
+        class: 'todo__checkbox-label',
+        for: this.item.id,
+      },
+      ['+'],
+    );
+    const checkbox = createElement('input', {
+      class: 'todo__checkbox-input',
+      type: 'checkbox',
+      id: this.item.id,
+    });
+
+    checkbox.checked = this.item.completed;
+
+    const checkboxWrapper = createElement(
+      'div',
+      {
+        class: 'todo__checkbox-wrapper',
+      },
+      [checkbox, label],
+    );
+
+    const inner = createElement('div', { class: 'todo__inner-text' }, [
+      this.item.inner,
+    ]);
+
+    const innerWrapper = createElement(
+      'div',
+      { class: 'todo__inner-wrapper' },
+      [inner],
+    );
+
+    const deleteButton = createElement(
+      'button',
+      { class: 'todo__delete-button' },
+      ['delete'],
+    );
+
+    const deleteButtonWrapper = createElement(
+      'div',
+      { class: 'todo__delete-wrapper' },
+      [deleteButton],
+    );
+
+    const todo = createElement('div', { class: 'todo' }, [
+      checkboxWrapper,
+      innerWrapper,
+      deleteButtonWrapper,
+    ]);
 
     // listeners
 
