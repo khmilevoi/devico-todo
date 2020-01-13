25426156bd9c44de038770a2d87556d1eaa619a7
khmilevoi <khmilevoi@gmail.com>
2020-01-13 16:19:55 +0200

    configure React for Webapack

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es6: true,
     node: true
   },
-  extends: ['airbnb-base'],
+  extends: ['airbnb-base', 'plugin:react/recommended'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly'
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import { mount } from 'shared/Component';
 
 import { logger } from 'middlewares/logger';
@@ -5,12 +8,16 @@ import { logger } from 'middlewares/logger';
 import { configureStore } from 'store/configureStore';
 import { initialState } from 'constants/initialState';
 
-import { App } from 'components/App';
+// import { App } from 'components/App';
 
 import 'styles/index.scss';
 
 const store = configureStore(initialState);
 store.subscribe(logger());
 
-const app = new App(store, document.body);
-document.body.append(mount(app));
+// const app = new App(store, document.body);
+// document.body.append(mount(app));
+
+const App = () => <div>Hello</div>;
+
+ReactDOM.render(<App></App>, document.getElementById('root'));
