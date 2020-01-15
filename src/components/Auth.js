e7f18cc1dc21c9deb645e879da59b1a4c00ef6ee
khmilevoi <khmilevoi@gmail.com>
2020-01-15 14:00:57 +0200

    feat complete auth component

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,21 +1,60 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
+import {
+  Paper,
+  Tabs,
+  Tab,
+  makeStyles,
+  TextField,
+  Button,
+} from '@material-ui/core';
+
 import { connect } from 'dux/connect';
 
 import { logIn, register } from 'store/actions/auth';
 
-const isActive = (state, type) => (state === type ? 'active' : '');
+// const isActive = (state, type) => (state === type ? 'active' : '');
+
+const useStyles = makeStyles({
+  paper: {
+    width: '100%',
+    height: '100%',
+    maxWidth: '500px',
+    maxHeight: '300px',
+    padding: '20px 15px',
+  },
+  tabs: {
+    width: 'calc(100% + 30px)',
+    margin: '-20px 0 0 -15px',
+  },
+  tab: {
+    width: '50%',
+  },
+});
 
 export const Auth = ({ error, logIn, register }) => {
-  const [state, setState] = useState('login');
+  const [state, setState] = useState(0);
   const [login, setLogin] = useState('');
   const [password, setPassword] = useState('');
 
+  const classes = useStyles();
+
   return (
     <div className="auth">
-      <div className="auth__wrapper">
-        <div className="auth__buttons">
+      {/* <div className="auth__wrapper"> */}
+      <Paper className={classes.paper}>
+        <Tabs
+          className={classes.tabs}
+          value={state}
+          indicatorColor="primary"
+          textColor="primary"
+          onChange={(_, newState) => setState(newState)}
+        >
+          <Tab className={classes.tab} label="Login"></Tab>
+          <Tab className={classes.tab} label="Register"></Tab>
+        </Tabs>
+        {/* <div className="auth__buttons">
           <button
             className={`auth__buttons-button ${isActive(state, 'login')}`}
             onClick={() => setState('login')}
@@ -28,20 +67,38 @@ export const Auth = ({ error, logIn, register }) => {
           >
             Register
           </button>
-        </div>
+        </div> */}
         <form
           className="auth__form"
           onSubmit={(event) => {
             event.preventDefault();
 
-            if (state === 'login') {
+            if (state === 0) {
               logIn(login, password);
-            } else if (state === 'register') {
+            } else if (state === 1) {
               register(login, password);
             }
           }}
         >
-          <input
+          <TextField
+            label="login"
+            variant="outlined"
+            type="login"
+            value={login}
+            onChange={(event) => setLogin(event.target.value)}
+          />
+          <TextField
+            label="password"
+            variant="outlined"
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <div className="auth__form-error">{error && error.message}</div>
+          <Button type="submit" variant="contained" color="primary">
+            Send
+          </Button>
+          {/* <input
             type="login"
             className="auth__form-input auth__form-item"
             placeholder="login"
@@ -55,12 +112,12 @@ export const Auth = ({ error, logIn, register }) => {
             value={password}
             onChange={(event) => setPassword(event.target.value)}
           />
-          <div className="auth__form-error">{error && error.message}</div>
           <button type="submit" className="auth__form-button auth__form-item">
             Send
-          </button>
+          </button> */}
         </form>
-      </div>
+      </Paper>
+      {/* </div> */}
     </div>
   );
 };
